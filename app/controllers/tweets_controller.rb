72cb783c6ad88fcf197890880b3700b57195e7c9
syuta sugawara <syuta.sugawara@gmail.com>
2016-09-30 07:46:52 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,66 @@
+class TweetsController < ApplicationController
+ before_action :set_tweet, only: [:edit, :update, :destroy]
+
+ 
+  def index
+    @tweets = Tweet.all
+  end
+  
+  def new
+     if params[:back]
+      @tweet = Tweet.new(blogs_params)
+    else
+      @tweet = Tweet.new
+    end
+  end
+
+  def create
+    @tweet=Tweet.create(tweets_params)
+    if @tweet.save
+    redirect_to tweets_path,notice: "ツイートしました！"
+  else
+    render action:'new'
+  end
+end
+
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(tweets_params)
+    
+     if @tweet.save
+    redirect_to tweets_path,notice: "ツイートの更新をしました！"
+     else
+    render action:'new'
+    end
+  end
+  
+  
+  
+  def destroy
+     @tweet = Tweet.find(params[:id])
+     @tweet.destroy
+      redirect_to tweets_path,notice: "ツイートを削除しました！"
+  end
+  
+  def confirm
+    @tweet = Tweet.new(tweets_params)
+   render :new if @tweet.invalid?
+  end
+  
+  
+  private
+  
+  def tweets_params
+    params.require(:tweet).permit(:content)
+  
+  end
+  
+  def set_tweet
+      @tweet = Tweet.find(params[:id])
+end
+end
